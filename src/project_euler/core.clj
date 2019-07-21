df8ef75dfa44967bc3575eb2711c77b395f41ebc
phosfox <mordredur@googlemail.com>
2019-07-21 13:15:50 +0200

    solves p30

---
+++ src/project_euler/core.clj
@@ -0,0 +1,7 @@
+(ns project-euler.core
+  (:require [project-euler.problems :as p])
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (p/p30))
---
+++ src/project_euler/problems.clj
@@ -0,0 +1,49 @@
+(ns project-euler.problems
+  (:import (java.lang Math)))
+
+(defn prime?
+  [n]
+  (every? false? (map #(= 0 (mod n %)) (range 2 (Math/sqrt n)))))
+
+(defn petersburg
+  []
+  (loop [payoff 1.0]
+    (if (> (rand)  0.5)
+      (recur (* 2 payoff))
+      payoff)))
+
+
+(defn p30
+  "Find the sum of all the numbers that can be written as the sum of fifth powers of their digits."
+  []
+  (reduce + 
+          (take 6 
+                (filter fifth-pow? (range)))))
+
+(defn fifth-pow?
+  [n]
+  (if (and (= n (sum-fifth-pow n)) (> n 1)) true))
+
+(defn sum-fifth-pow
+  [n]
+  (->> n
+       digits
+       (map #(pow % 5))
+       (reduce +)))
+
+(defn digits [n]
+  (->> n
+       str
+       (map (comp read-string str))))
+
+(defn digits2 [n]
+  (->> n
+       (iterate #(quot % 10))
+       (take-while pos?)
+       (mapv #(mod % 10))
+       rseq))
+
+(defn pow
+  "Returns the result of n^x"
+  [n x]
+  (reduce * (repeat x n)))
